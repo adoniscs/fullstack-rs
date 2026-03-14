7687a2182eb858bd894b583a9c06dc9485deb731
Adonis Silveira <desenvolvimento777@gmail.com>
2026-03-13 22:16:10 -0300

    create server.ts

---
+++ api-restaurant/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { routes } from "./routes";
+import { errorHandling } from "./middlewares/error-handlings";
+
+const PORT = 3333;
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(errorHandling);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
