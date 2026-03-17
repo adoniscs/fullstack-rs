34d77bf4f5588d220faa3866876dc2d6ec317613
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-03-17 20:38:46 -0300

    Criado tabela para mesas e populado os dados para ela

--- api-restaurant/src/database/migrations/20260317232056_create-tables.ts
+++ api-restaurant/src/database/migrations/20260317232056_create-tables.ts
@@ -5,10 +5,10 @@ export async function up(knex: Knex): Promise<void> {
     (table.increments("id").primary(),
       table.integer("table_number").notNullable(),
       table.timestamp("created_at").defaultTo(knex.fn.now()),
-      table.timestamp("updated_at").defaultTo(knex.fn.now()));
+      table.timestamp("updated_ad").defaultTo(knex.fn.now()));
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable("tables")
+  await knex.schema.dropTable("tables");
 }
