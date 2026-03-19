4bc35ae5d25751b5142e56d075756382aa1a557c
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-03-18 22:22:48 -0300

    Refinando abertura das mesas

--- api-restaurant/src/controllers/tables-sessions-controller.ts
+++ api-restaurant/src/controllers/tables-sessions-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { knex } from "@/database/knex";
 import { z } from "zod";
+import {AppError} from "@/utils/app-error";
 
 class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
@@ -10,6 +11,17 @@ class TablesSessionsController {
       });
 
       const { table_id } = bodySchema.parse(request.body);
+
+      const session = await knex<TablesSessionsRepository>("tables_session")
+        .where({
+          table_id,
+        })
+        .orderBy("opened_at", "desc")
+        .first();
+
+      if (session && !session.closed_at) {
+        throw new AppError("this table is already open")
+      }
 
       await knex<TablesSessionsRepository>("tables_session").insert({
         table_id,
