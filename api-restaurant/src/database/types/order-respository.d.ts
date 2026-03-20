de92b337424321ebdec57c7e5b8def60ee3eb3b6
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-03-19 21:12:52 -0300

    Criação dos tipos para tabela de pedidos

--- api-restaurant/src/database/types/order-respository.d.ts
+++ api-restaurant/src/database/types/order-respository.d.ts
@@ -1,9 +1,9 @@
 type OrderRespository = {
-  id: number
-  table_session_id: number
-  product_id: number
-  quantity: number
-  price: number
-  created_at: number
-  updated_at: number
-}
+  id: number;
+  table_session_id: number;
+  product_id: number;
+  quantity: number;
+  price: number;
+  created_at: number;
+  updated_at: number;
+};
