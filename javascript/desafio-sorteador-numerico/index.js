3c5802ed84e649acafea50c7a3c0e452e5c0cee6
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-03-26 12:54:40 -0300

    Arquivo principal que estancia a classe NumeroSorteado para gerar os números para o sorteio

--- javascript/desafio-sorteador-numerico/index.js
+++ javascript/desafio-sorteador-numerico/index.js
@@ -1,35 +1,4 @@
-class GeradorDeNumeros {
-  constructor(numeroMinimo, numeroMaximo) {
-    this.numeroMinimo = Math.ceil(numeroMinimo);
-    this.numeroMaximo = Math.floor(numeroMaximo);
-  }
+import NumeroSorteado from "./NumeroSorteado.js";
 
-  gerar() {
-    return (
-      Math.floor(Math.random() * (this.numeroMaximo - this.numeroMinimo + 1)) +
-      this.numeroMinimo
-    );
-  }
-}
-
-class NumeroSorteado extends GeradorDeNumeros {
-  numerosSorteados = [];
-  numero = 0;
-
-  constructor(quantidade, minimo, maximo) {
-    super(minimo, maximo);
-    this.quantidade = quantidade;
-  }
-
-  sorteio() {
-    for (let i = 0; i < this.quantidade; i++) {
-      this.numero = super.gerar();
-      this.numerosSorteados.push(this.numero);
-    }
-
-    return this.numerosSorteados;
-  }
-}
-
-const novoSorteio = new NumeroSorteado(5, 1, 100);
+const novoSorteio = new NumeroSorteado(4, 1, 10, false);
 console.log(novoSorteio.sorteio());
