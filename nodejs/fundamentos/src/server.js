0ba5690974fec6c9e3ea4b6cc4eb96f655ada402
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-03-28 11:04:57 -0300

    Criado servidor e rotas GET e POST

--- nodejs/fundamentos/src/server.js
+++ nodejs/fundamentos/src/server.js
@@ -1,1 +1,27 @@
+import http from "node:http";
 
+const PORT = process.env.PORT || 3333;
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/products") {
+    return res.end("Lista de produtos");
+  }
+
+  if (method === "POST" && url === "/products") {
+    const buffers = [];
+
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+
+    console.log(Buffer.concat(buffers).toString());
+
+    return res.writeHead(201).end("Produto cadastrado!");
+  }
+
+  return res.writeHead(404).end("Produto não encontrado");
+});
+
+server.listen(PORT);
