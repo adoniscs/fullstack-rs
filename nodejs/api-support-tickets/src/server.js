4fafab835a0d018f733a9ff70387819325812bb9
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-03-29 12:00:55 -0300

    Importa os middlewares e utiliza os recursos deles

--- nodejs/api-support-tickets/src/server.js
+++ nodejs/api-support-tickets/src/server.js
@@ -1,7 +1,12 @@
 import http from "node:http";
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
 
 const PORT = process.env.PORT || 3333;
 
-function listener(req, res) {}
+async function listener(req, res) {
+  await jsonHandler(req, res);
+  routeHandler(req, res);
+}
 
 http.createServer(listener).listen(PORT);
