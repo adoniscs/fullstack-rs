587142f83c5e814eb8a8e1f9d82e90fb1049d0e2
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-03-29 21:20:56 -0300

    Criado rodas e validação para filtrar os tickets pelo status

--- nodejs/api-support-tickets/src/controllers/tickets/index.js
+++ nodejs/api-support-tickets/src/controllers/tickets/index.js
@@ -1,5 +1,8 @@
-export function index ({ req, res, database}) {
-    const tickets = database.select("tickets");
+export function index({ req, res, database }) {
+  const { status } = req.query;
+  const filters = status ? { status } : null;
 
-    return res.end(JSON.stringify(tickets));
+  const tickets = database.select("tickets", filters);
+
+  return res.end(JSON.stringify(tickets));
 }
--- nodejs/api-support-tickets/src/database/database.js
+++ nodejs/api-support-tickets/src/database/database.js
@@ -1,36 +1,44 @@
-import fs from "node:fs/promises"
+import fs from "node:fs/promises";
 
 const DATABASE_PATH = new URL("db.json", import.meta.url);
 
 export class Database {
-    #database = {}
+  #database = {};
 
-    constructor() {
-        fs.readFile(DATABASE_PATH, "utf8")
-            .then((data) => {
-                this.#database = JSON.parse(data);
-            }).catch(() => {
-            this.#persist();
-        });
-    }
-
-    #persist() {
-        fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));
-    }
-
-    insert(table, data) {
-        if(Array.isArray(this.#database[table])) {
-            this.#database[table].push(data);
-        } else {
-            this.#database[table] = [data]
-        }
-
+  constructor() {
+    fs.readFile(DATABASE_PATH, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
         this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
     }
 
-    select(table) {
-        let data = this.#database[table] ?? [];
+    this.#persist();
+  }
 
-        return data;
+  select(table, filters) {
+    let data = this.#database[table] ?? [];
+
+    if (filters) {
+      data = data.filter((row) => {
+        return Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
     }
+    return data;
+  }
 }
