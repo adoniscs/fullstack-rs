27bfdb48c23dd71a6c855b41f33b4fa56d899fc7
Adonis Silveira <desenvolvimento777@gmail.com>
2026-04-15 18:06:55 -0300

    Criado estrutura para projeto agenda de contatos

---
+++ javascript/agenda_de_contato/index.js
@@ -0,0 +1,54 @@
+import readline from "node:readline/promises";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const listaDeContatos = [];
+
+async function menu() {
+    const opcao = await rl.question(
+        `
+    1. Adicionar um contato
+    2. Listar todos os contatos
+    3. Editar um contato
+    4. Favoritar um contato
+    5. Visualizar contato favoritos
+    6. Deletar um contato
+    7. Sair
+    `,
+    );
+
+    switch (opcao) {
+        case "1":
+            console.log("Adicionar contato");
+
+            break;
+        case "2":
+            console.log("Listar todos os contatos");
+            break;
+        case "3":
+            console.log("Editar um contato");
+            break;
+        case "4":
+            console.log("Favoritar um contato");
+            break;
+        case "5":
+            console.log("Visualizar contato favoritos");
+            break;
+        case "6":
+            console.log("Deletar um contato");
+            break;
+        case "7":
+            console.log("Sair");
+            rl.close();
+            break;
+        default:
+            console.log("Opção inválida. Tente novamente.");
+            menu();
+            break;
+    }
+}
+
+menu();
